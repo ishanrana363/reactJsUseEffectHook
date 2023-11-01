53195a3932974f61c727569aa6fbb18e245ebf0e
ishanrana363 <ishanrana363@gmail.com>
2023-11-01 22:28:57 +0600

    useEffect understing arguments and uses

--- src/Component/UseEffectUndersting.jsx
+++ src/Component/UseEffectUndersting.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const UseEffectUndersting = () => {
+     useEffect(()=>{
+         console.log('Hello world')
+     },[4,6,57])
     return (
         <div>
             
